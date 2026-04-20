8ca50f4c9f9d3eb8ee43b1719b30a10b610171f4
vboxuser <vboxuser@Ubuntu25.10>
2026-04-19 23:49:54 -0500

    Added CI tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,8 +3,11 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
+def test_add():
+    assert 2 + 2 == 4
 
-
+def test_subtract():
+    assert 4 - 2 == 2
 ###
 
 print("All tests passed!")
